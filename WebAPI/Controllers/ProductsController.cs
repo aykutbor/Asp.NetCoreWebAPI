0d2c9de4f12c97839fed0dff182b81d3ba448fec
Aykut BÖR <117044446+aykutbor@users.noreply.github.com>
2025-07-28 23:38:03 +0300

    Add project files.

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string Hello([FromQuery]  string name, [FromQuery] string surname)   // url de tanımlanırken ad=ahmet&soyad=dev şeklinde yazılır.
+        {
+            return "Hello" + name + "" + surname;
+        }
+
+        [HttpGet("tobeto")]
+        public string Tobeto()
+        {
+            return "Tobeto";
+        }
+
+        [HttpGet("{username}")]
+        public string Course([FromRoute]string username)
+        {
+            return "Course";
+        }
+
+        [HttpPost]
+        public string GoodBye([FromBody] Product product)
+        {
+            return "GoodBye";
+        }
+    }
+}
+
+// Route Parameters, Query String => Get isteklerinde popüler.
+// Body => POST, PUT.
+// Headers => Yan bilgileri içerir (İstek nereden atıldı? İstek atılırken kullanılan dil neydi? Hangi dil tercih ediliyor? şeklindedir.
+ 
