4502f58ea886e8169b843bb6e3d49a46dba2e522
Aykut BÖR <117044446+aykutbor@users.noreply.github.com>
2025-08-09 13:25:50 +0300

    Seed Data

--- DataAccess/Concretes/Entity Framework/BaseDbContext.cs
+++ DataAccess/Concretes/Entity Framework/BaseDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,20 +16,37 @@ namespace DataAccess.Concretes.Entity_Framework
         public DbSet<Category> Categories { get; set; }
 
 
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    optionsBuilder
-        //        .UseSqlServer(
-        //            @"Server=(localdb)\MSSQLLocalDB; Database=DbWebApi; Trusted_Connection=True;",
-        //            options => options.EnableRetryOnFailure());
-        //}
-
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Database=DbWebApi2; Trusted_Connection=True;");
             base.OnConfiguring(optionsBuilder);
 
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().ToTable("Products"); // Tablo ismi değiştirilir.
+            modelBuilder.Entity<Product>().HasOne(i => i.Category); // Tablo ilişkileri kurulur.
+            modelBuilder.Entity<Product>().Property(i => i.Name).HasColumnName("Name").HasMaxLength(50); // Tablodaki kolon ismi değiştirilebilir.
+
+
+            // OnModelCreating -> Genel veritabanı özellikleriyle değil, direkt modelin özellikleriyle çalışan metottur. Özetle, veritabanındaki özelleştirmelerimizi yapıyoruz.
+
+
+            // Seed Data -> Sizin veritabanını oluştururken kullanabileceğiniz test verilerini otomatik eklemek demektir. Bu örnek veriler veritabanında varsayılan olarak görünür.(Migration devreye alındığı zaman). CodeFirst yapılarında db'den veri oluşturmamak için test amaçlı kullanılır.
+
+            // Aşağıdaki yapı Seed Data'dır.
+            Category category = new Category(1, "Giyim");
+            Category category2 = new(2, "Elektronik");
+
+            Product product = new Product(1, "Kazak", 500, 50, 1);
+
+
+            modelBuilder.Entity<Category>().HasData(category, category2);
+            modelBuilder.Entity<Product>().HasData(product);
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
@@ -37,3 +55,4 @@ namespace DataAccess.Concretes.Entity_Framework
 // override OnModelCreating => İlgili databse'in altındaki modeller create edilirken çalışan metot.
 
 // optionsBuilder, kullanarak veritabanı options'ları set edilebilir.
+
